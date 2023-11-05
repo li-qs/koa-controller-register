810902698b2defd4641a8f5137ffb810e62bcc02
Li Qiushi <li.qs@foxmail.com>
2023-11-06 00:31:01 +0800

    Update

--- example/index.ts
+++
@@ -1,9 +0,0 @@
-import Application from 'koa'
-import PingController from './ping.controller'
-import { registerControllers } from '../src'
-
-const controllers = [PingController]
-const app = new Application()
-// ...
-registerControllers(app, controllers)
-app.listen(8080)
